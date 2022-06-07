14e104ca9780682cb4c1bb7529fb1c5c12ca85d5
Erick Paiva <123skatesk8@gmail.com>
2022-06-07 16:28:46 -0400

    feat: added login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import {
-  ChakraProvider,
-  Box,
-  theme,
-} from "@chakra-ui/react"
+import { ChakraProvider, Box, theme } from "@chakra-ui/react";
+import Login from "./pages/login";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      asd
-    </Box>
+    <Login />
   </ChakraProvider>
-)
+);

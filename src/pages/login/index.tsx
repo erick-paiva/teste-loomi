df09f8b16fc0abe65803c21c70890fcdd684b896
Erick Paiva <123skatesk8@gmail.com>
2022-06-07 16:30:33 -0400

    feat: created the base of the login page

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,27 @@
+import { Box, Center, Flex, Heading, Img } from "@chakra-ui/react";
+import BackgroundImage from "../../assets/imagesPage/backgroundImage.svg";
+import LogoLoomi from "../../assets/imagesPage/logoLoomi.svg";
+
+const Login = () => {
+  return (
+    <Center
+      h="100vh"
+      w="100vw"
+      backgroundImage={BackgroundImage}
+      backgroundRepeat="no-repeat"
+      backgroundPosition="center"
+      backgroundSize="cover"
+    >
+      <Center h="100%" w="894px" bg="white">
+        <Center flexDirection="column">
+          <Img src={LogoLoomi} alt="logoLoomi" />
+          <Heading as="h3" size="xl">
+            Entrar na plataforma
+          </Heading>
+        </Center>
+      </Center>
+    </Center>
+  );
+};
+
+export default Login;

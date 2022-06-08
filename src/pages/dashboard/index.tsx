284a038847e8a8b8323b5c33c7a851744288c27f
Erick Paiva <123skatesk8@gmail.com>
2022-06-08 14:33:20 -0400

    feat: I added the header component to the dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,23 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex } from "@chakra-ui/react";
+import Header from "../../components/header";
+import BackgroundImage from "../../assets/imagesPage/backgroundImage.svg";
+import LogoLoomi from "../../assets/imagesPage/logoLoomi.svg";
 
 
 const Dashboard = () => {
+  return (
+    <Flex
+      h="100vh"
+      w="100vw"
+      backgroundImage={BackgroundImage}
+      backgroundRepeat="no-repeat"
+      backgroundPosition="center"
+      backgroundSize="cover"
+      bgColor="purple.50"
+    >
+      <Header userName="eduardo"/>
+    </Flex>
+  );
+};
 
-
-    return (
-        <Flex>
-            Dashboard
-        </Flex>
-    )
-}
-
-export default Dashboard
+export default Dashboard;

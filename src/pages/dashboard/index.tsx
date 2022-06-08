b3b38c447789b479cb95292167614701f5b06801
Erick Paiva <123skatesk8@gmail.com>
2022-06-08 14:32:11 -0400

    feat: I created the base of the dashboard page

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,14 @@
+import { Flex } from "@chakra-ui/react"
+
+
+const Dashboard = () => {
+
+
+    return (
+        <Flex>
+            Dashboard
+        </Flex>
+    )
+}
+
+export default Dashboard

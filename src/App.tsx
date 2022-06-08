4936934a402068da3e5860651d3433b49c37751b
Erick Paiva <123skatesk8@gmail.com>
2022-06-07 23:10:50 -0400

    fix: removed chakraprovider from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { ChakraProvider, Box, theme } from "@chakra-ui/react";
 import Login from "./pages/login";
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
     <Login />
-  </ChakraProvider>
 );

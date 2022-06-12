73c12654514c20bc4551b0fecf3fd4b690bac41c
Erick Paiva <123skatesk8@gmail.com>
2022-06-11 23:27:16 -0400

    feat: added memo function to improve component performance

--- src/components/charts/index.tsx
+++ src/components/charts/index.tsx
@@ -1,8 +1,8 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box} from "@chakra-ui/react";
+import { memo, useEffect, useState } from "react";
 import ReactApexChart from "react-apexcharts";
 import SelectStart from "./select";
 import "./styles.css";
-
 interface Props {
   data: {
     options?: any;
@@ -12,12 +12,15 @@ interface Props {
     width?: number | string;
   };
   showSelect?: boolean;
+  stylize?: boolean;
 }
 
 const ChartOrdersMonth: React.FC<Props> = ({
   data,
   showSelect = true,
+  stylize = false,
 }: Props) => {
+
   const {
     options,
     series,
@@ -34,7 +37,7 @@ const ChartOrdersMonth: React.FC<Props> = ({
         series={series}
         height={height}
         width={width}
-        id="chart"
+        {...(stylize && {id: "chart"})}
       />
       {showSelect && (
         <SelectStart
@@ -67,4 +70,4 @@ const ChartOrdersMonth: React.FC<Props> = ({
     </Box>
   );
 };
-export default ChartOrdersMonth;
+export default memo(ChartOrdersMonth);

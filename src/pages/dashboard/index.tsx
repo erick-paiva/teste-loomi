d1cdcac60a37e1b448f5060b3fcdb97cae66e73b
Erick Paiva <123skatesk8@gmail.com>
2022-06-13 17:05:32 -0400

    feat: I relocated the sessions to improve readability

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,22 +2,13 @@ import { Box, Flex, Heading, HStack, keyframes } from "@chakra-ui/react";
 import Header from "../../components/header";
 import BackgroundImage from "../../assets/imagesPage/backgroundImage.svg";
 import Sidebar from "../../components/sidebar";
-import Chart from "../../components/charts";
-import { profitExpectation } from "./chartOptions/profitExpectation";
-import { orderPerMonth } from "./chartOptions/orderPerMonth";
-import { braidsByAge } from "./chartOptions/braidsByAge";
-import { ordersMade } from "./chartOptions/ordersMade";
-import { categoriesOrders } from "./chartOptions/categoriesOrders";
-import { sessionsByGenre } from "./chartOptions/sessionsByGenre";
-import { transactionByCustomerType } from "./chartOptions/transactionByCustomerType";
-import { transactionByDevice } from "./chartOptions/transactionByDevice";
-import { useEffect, useState } from "react";
 import ProductsTable from "./productsTable";
 import SessionHome from "./sessionHome";
 import FunnelSession from "./funnelSession";
+import SalesDashboardSession from "./salesDashboardSession";
+import UserProfileSession from "./userProfileSession";
 
 const Dashboard = () => {
-
   const scrollBarStyle = {
     "&::-webkit-scrollbar": {
       width: "15px",
@@ -80,42 +71,7 @@ const Dashboard = () => {
               </Heading>
               <SessionHome />
             </Box>
-            <Box mb="40px" minW="80%" mr="40px">
-              <Heading
-                as="h3"
-                color="purple.500"
-                letterSpacing="0.48px"
-                fontSize="24px"
-                fontWeight="bold"
-                mb="32px"
-                ml="40px"
-              >
-                Dashboard de vendas
-              </Heading>
-              <HStack
-                overflowX="auto"
-                css={scrollBarStyle}
-                spacing="32px"
-                minH="200px"
-              >
-                <Box bg="white" borderRadius="12px">
-                  <Chart data={orderPerMonth()} />
-                </Box>
-                <Box bg="white" borderRadius="12px">
-                  <Chart data={profitExpectation()} />
-                </Box>
-                <Box bg="white" borderRadius="12px">
-                  <Chart data={ordersMade()} />
-                </Box>
-                <Box bg="white" borderRadius="12px">
-                  <Chart
-                    data={categoriesOrders()}
-                    showSelect={false}
-                    stylize={true}
-                  />
-                </Box>
-              </HStack>
-            </Box>
+            <SalesDashboardSession />
             <Box mb="40px" minW="80%" mr="40px">
               <Heading
                 as="h3"
@@ -138,55 +94,8 @@ const Dashboard = () => {
               </HStack>
             </Box>
 
-            <Box mb="40px" minW="80%" mr="40px" css={scrollBarStyle}>
-              <Heading
-                as="h3"
-                color="purple.500"
-                letterSpacing="0.48px"
-                fontSize="24px"
-                fontWeight="bold"
-                mb="32px"
-                ml="40px"
-              >
-                Perfil do usuário
-              </Heading>
-              <HStack
-                overflowX="auto"
-                css={scrollBarStyle}
-                spacing="32px"
-                minH="200px"
-              >
-                <Box bg="white" borderRadius="12px">
-                  <Chart
-                    data={braidsByAge()}
-                    showSelect={false}
-                    stylize={true}
-                  />
-                </Box>
+            <UserProfileSession />
 
-                <Box bg="white" borderRadius="12px">
-                  <Chart
-                    data={sessionsByGenre()}
-                    showSelect={false}
-                    stylize={true}
-                  />
-                </Box>
-                <Box bg="white" borderRadius="12px">
-                  <Chart
-                    data={transactionByCustomerType()}
-                    showSelect={false}
-                    stylize={true}
-                  />
-                </Box>
-                <Box bg="white" borderRadius="12px">
-                  <Chart
-                    data={transactionByDevice()}
-                    showSelect={false}
-                    stylize={true}
-                  />
-                </Box>
-              </HStack>
-            </Box>
             <ProductsTable />
           </Box>
         </Flex>
